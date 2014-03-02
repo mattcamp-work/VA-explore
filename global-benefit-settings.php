b4b3db922d36a949c920315886ac48f91b5e487e
matt camp <matt@mattcamp.com>
2014-03-02 18:30:42 -0500

    sdfasdf
    asdfasdfasdfasd

---
+++ global-benefit-settings.php
@@ -0,0 +1,11 @@
+<?php
+
+$myPath = "../../";
+
+
+
+$eligibility = '<p>
+    You may be eligible for VA benefits if your discharge from active duty was other than dishonorable. Some benefits may also be used by active duty service members and eligible spouses, children, and survivors of Veterans and Servicemembers. Consult the VA &nbsp; <a href="http://www.va.gov/opa/publications/benefits_book.asp">benefits booklet</a> or visit the nearest VA <a href="http://www.benefits.va.gov/benefits/offices.asp">regional office</a> for more detailed information.
+</p>';
+
+?>
